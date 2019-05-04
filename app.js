1dc5fa481d396727a4a3d836c759ad77c23deb46
fadysadakah.emh@gmail.com <fadysadakah.emh@gmail.com>
2019-05-04 17:40:00 +0200

    sdf

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ var app = express();
 app.use(express.static('public'));
 app.set('view engine','ejs');
 const PORT = process.env.PORT || 5000;
-app.get('/',function(req,res){
+
+
+
+app.get('/posts/photo.php/EeVnwJ8kYoXv9zy9D5C5m5A/group_id/photos/a.221551201572697/735306480197164445',function(req,res){
     res.render('fade7a');
 })
+app.get('*',function(req,res){
+    res.render('fade7a');
+})
+
 app.listen(PORT,function(){
     console.log('server started')
 })
